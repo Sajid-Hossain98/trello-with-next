348d5f1f0f58f2860a14fb80c959f23f768be1a8
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-11-24 18:23:45 +0600

    Marketing page navbar

--- components/logo.tsx
+++ components/logo.tsx
@@ -13,9 +13,7 @@ export const Logo = () => {
       <div className="hover:opacity-75 transition hidden md:flex items-center gap-x-2">
         <Image src="/logo.svg" alt="logo" height={30} width={30} />
 
-        <p
-          className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
-        >
+        <p className={cn("text-lg text-neutral-700", headingFont.className)}>
           To-dofy
         </p>
       </div>
