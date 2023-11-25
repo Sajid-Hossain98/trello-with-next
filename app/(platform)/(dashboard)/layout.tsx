af5262272a49ba0e59237fdd1a212eaa6b084b0c
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-11-25 23:11:04 +0600

    dashboard navbar

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -1,5 +1,12 @@
+import { Navbar } from "./_components/navbar";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="h-full">
+      <Navbar />
+      {children}
+    </div>
+  );
 };
 
 export default DashboardLayout;
