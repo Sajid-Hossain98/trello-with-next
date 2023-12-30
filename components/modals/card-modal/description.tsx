0cf63f02c4e62e4e299e88890ccf128157461a75
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-12-30 19:13:49 +0600

    revalidating card-logs for auditLogs

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -54,6 +54,11 @@ export const Description = ({ data }: DescriptionProps) => {
       queryClient.invalidateQueries({
         queryKey: ["card", data.id],
       });
+
+      queryClient.invalidateQueries({
+        queryKey: ["card-logs", data.id],
+      });
+
       toast.success(`Card description for "${data.title}" updated`);
       disableEditing();
     },
